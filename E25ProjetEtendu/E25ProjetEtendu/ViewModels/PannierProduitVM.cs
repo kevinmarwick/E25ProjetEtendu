d3d3f1835cf273b5e7448aa01e4a77d7aa1df6d0
Gab <6221324@cegepmontpetit.ca>
2025-04-30 17:00:01 -0400

    TACHE 473

---
+++ E25ProjetEtendu/E25ProjetEtendu/ViewModels/PannierProduitVM.cs
@@ -0,0 +1,10 @@
+﻿namespace E25ProjetEtendu.ViewModels
+{
+    public class PannierProduitVM
+    {
+        public int ProduitId { get; set; }
+        public string Nom { get; set; }
+        public decimal Prix { get; set; }
+        public int Quantite { get; set; }
+    }
+}
---
+++ E25ProjetEtendu/E25ProjetEtendu/ViewModels/PannierVM.cs
@@ -0,0 +1,9 @@
+﻿namespace E25ProjetEtendu.ViewModels
+{
+    public class PannierVM
+    {
+        public List<PannierProduitVM> Produits { get; set; } = new();
+
+        public decimal Total => Produits.Sum(i => i.Prix * i.Quantite);
+    }
+}
