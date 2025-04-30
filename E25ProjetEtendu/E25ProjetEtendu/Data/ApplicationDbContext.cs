4ca5a1270612e6fe4501ba1999973eecadd9f2cb
Gab <6221324@cegepmontpetit.ca>
2025-04-30 11:06:22 -0400

    FCT seed de produit en bd

--- E25ProjetEtendu/E25ProjetEtendu/Data/ApplicationDbContext.cs
+++ E25ProjetEtendu/E25ProjetEtendu/Data/ApplicationDbContext.cs
@@ -1,12 +1,134 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using E25ProjetEtendu.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace E25ProjetEtendu.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Produit> produits { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); // important à garder
+
+            modelBuilder.Entity<Produit>().HasData(
+                new Produit
+                {
+                    id = 1,
+                    nom = "Red Bull",
+                    Qty = 120,
+                    prix = 3,
+                    Image = "redbull.jpg",
+                    estActif = true,
+                    note = 4,
+                    valeurNutritive = "Calories: 110, Sucres: 27g, Caféine: 80mg, Glucides: 28g, Protéines: 1g"
+                },
+    new Produit
+    {
+        id = 2,
+        nom = "Pogo",
+        Qty = 200,
+        prix = 2,
+        Image = "pogo.jpg",
+        estActif = true,
+        note = 3,
+        valeurNutritive = "Calories: 190, Lipides: 9g, Glucides: 20g, Protéines: 6g, Sodium: 500mg"
+    },
+    new Produit
+    {
+        id = 3,
+        nom = "Bouteille d'eau",
+        Qty = 300,
+        prix = 1,
+        Image = "eau.jpg",
+        estActif = true,
+        note = 5,
+        valeurNutritive = "Calories: 0, Lipides: 0g, Sucres: 0g, Sodium: 0mg"
+    },
+    new Produit
+    {
+        id = 4,
+        nom = "Chips Lay’s",
+        Qty = 100,
+        prix = 2,
+        Image = "chips.jpg",
+        estActif = true,
+        note = 4,
+        valeurNutritive = "Calories: 160, Lipides: 10g, Glucides: 15g, Sucres: 1g, Sodium: 170mg"
+    },
+    new Produit
+    {
+        id = 5,
+        nom = "Nutella",
+        Qty = 80,
+        prix = 5,
+        Image = "nutella.jpg",
+        estActif = true,
+        note = 5,
+        valeurNutritive = "Calories: 200, Lipides: 11g, Glucides: 22g, Sucres: 21g, Protéines: 2g"
+    },
+    new Produit
+    {
+        id = 6,
+        nom = "Yogourt Activia",
+        Qty = 150,
+        prix = 3,
+        Image = "activia.jpg",
+        estActif = true,
+        note = 4,
+        valeurNutritive = "Calories: 100, Lipides: 2g, Glucides: 15g, Sucres: 12g, Protéines: 5g"
+    },
+    new Produit
+    {
+        id = 7,
+        nom = "Pizza congelée",
+        Qty = 60,
+        prix = 6,
+        Image = "pizza.jpg",
+        estActif = true,
+        note = 4,
+        valeurNutritive = "Calories: 350, Lipides: 15g, Glucides: 40g, Sucres: 5g, Protéines: 12g"
+    },
+    new Produit
+    {
+        id = 8,
+        nom = "Barre de granola",
+        Qty = 180,
+        prix = 2,
+        Image = "granola.jpg",
+        estActif = true,
+        note = 4,
+        valeurNutritive = "Calories: 190, Lipides: 7g, Glucides: 29g, Sucres: 11g, Protéines: 4g"
+    },
+    new Produit
+    {
+        id = 9,
+        nom = "Coca-Cola",
+        Qty = 220,
+        prix = 2,
+        Image = "coca.jpg",
+        estActif = true,
+        note = 3,
+        valeurNutritive = "Calories: 140, Sucres: 39g, Glucides: 39g, Sodium: 45mg"
+    },
+    new Produit
+    {
+        id = 10,
+        nom = "Sandwich jambon-fromage",
+        Qty = 75,
+        prix = 4,
+        Image = "sandwich.jpg",
+        estActif = true,
+        note = 4,
+        valeurNutritive = "Calories: 320, Lipides: 12g, Glucides: 30g, Protéines: 18g, Sodium: 780mg"
+    }
+               
+            );
         }
     }
 }
---
+++ E25ProjetEtendu/E25ProjetEtendu/Migrations/20250430145603_SeedProduit.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace E25ProjetEtendu.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProduit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "produits",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nom = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Qty = table.Column<int>(type: "int", nullable: false),
+                    prix = table.Column<int>(type: "int", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    estActif = table.Column<bool>(type: "bit", nullable: false),
+                    note = table.Column<int>(type: "int", nullable: false),
+                    valeurNutritive = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_produits", x => x.id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "produits",
+                columns: new[] { "id", "Image", "Qty", "estActif", "nom", "note", "prix", "valeurNutritive" },
+                values: new object[,]
+                {
+                    { 1, "redbull.jpg", 120, true, "Red Bull", 4, 3, "Calories: 110, Sucres: 27g, Caféine: 80mg, Glucides: 28g, Protéines: 1g" },
+                    { 2, "pogo.jpg", 200, true, "Pogo", 3, 2, "Calories: 190, Lipides: 9g, Glucides: 20g, Protéines: 6g, Sodium: 500mg" },
+                    { 3, "eau.jpg", 300, true, "Bouteille d'eau", 5, 1, "Calories: 0, Lipides: 0g, Sucres: 0g, Sodium: 0mg" },
+                    { 4, "chips.jpg", 100, true, "Chips Lay’s", 4, 2, "Calories: 160, Lipides: 10g, Glucides: 15g, Sucres: 1g, Sodium: 170mg" },
+                    { 5, "nutella.jpg", 80, true, "Nutella", 5, 5, "Calories: 200, Lipides: 11g, Glucides: 22g, Sucres: 21g, Protéines: 2g" },
+                    { 6, "activia.jpg", 150, true, "Yogourt Activia", 4, 3, "Calories: 100, Lipides: 2g, Glucides: 15g, Sucres: 12g, Protéines: 5g" },
+                    { 7, "pizza.jpg", 60, true, "Pizza congelée", 4, 6, "Calories: 350, Lipides: 15g, Glucides: 40g, Sucres: 5g, Protéines: 12g" },
+                    { 8, "granola.jpg", 180, true, "Barre de granola", 4, 2, "Calories: 190, Lipides: 7g, Glucides: 29g, Sucres: 11g, Protéines: 4g" },
+                    { 9, "coca.jpg", 220, true, "Coca-Cola", 3, 2, "Calories: 140, Sucres: 39g, Glucides: 39g, Sodium: 45mg" },
+                    { 10, "sandwich.jpg", 75, true, "Sandwich jambon-fromage", 4, 4, "Calories: 320, Lipides: 12g, Glucides: 30g, Protéines: 18g, Sodium: 780mg" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "produits");
+        }
+    }
+}
