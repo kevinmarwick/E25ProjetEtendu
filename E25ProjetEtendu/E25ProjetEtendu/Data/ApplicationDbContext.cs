c5dae9344acabcab05a0ffed5df969a5eb7fbabb
Gabriel <6221324@cegepmontpetit.ca>
2025-05-07 11:55:50 -0400

    Migration Produit
    Ajout de date au model produit + migration

--- E25ProjetEtendu/E25ProjetEtendu/Data/ApplicationDbContext.cs
+++ E25ProjetEtendu/E25ProjetEtendu/Data/ApplicationDbContext.cs
@@ -50,121 +50,132 @@ namespace E25ProjetEtendu.Data
             #endregion
 
 
-
+            #region Ajout Produit
 
             modelBuilder.Entity<Produit>().HasData(
-            new Produit
-            {
-                ProduitId = 1,
-                Nom = "Red Bull",
-                Qty = 120,
-                Prix = 3,
-                Image = "redbull.png",
-                EstActif = true,
-                Note = 4,
-                ValeurNutritive = "Calories: 110, Sucres: 27g, Caféine: 80mg, Glucides: 28g, Protéines: 1g"
-            },
-            new Produit
-            {
-                ProduitId = 2,
-                Nom = "Pogo",
-                Qty = 200,
-                Prix = 2,
-                Image = "pogo.jpg",
-                EstActif = true,
-                Note = 3,
-                ValeurNutritive = "Calories: 190, Lipides: 9g, Glucides: 20g, Protéines: 6g, Sodium: 500mg"
-            },
-            new Produit
-            {
-                ProduitId = 3,
-                Nom = "Bouteille d'eau",
-                Qty = 300,
-                Prix = 1,
-                Image = "eau.jpg",
-                EstActif = true,
-                Note = 5,
-                ValeurNutritive = "Calories: 0, Lipides: 0g, Sucres: 0g, Sodium: 0mg"
-            },
-            new Produit
-            {
-                ProduitId = 4,
-                Nom = "Chips Lay’s",
-                Qty = 100,
-                Prix = 2,
-                Image = "chips.jpg",
-                EstActif = true,
-                Note = 4,
-                ValeurNutritive = "Calories: 160, Lipides: 10g, Glucides: 15g, Sucres: 1g, Sodium: 170mg"
-            },
-            new Produit
-            {
-                ProduitId = 5,
-                Nom = "Nutella",
-                Qty = 80,
-                Prix = 5,
-                Image = "nutella.jpg",
-                EstActif = true,
-                Note = 5,
-                ValeurNutritive = "Calories: 200, Lipides: 11g, Glucides: 22g, Sucres: 21g, Protéines: 2g"
-            },
-            new Produit
-            {
-                ProduitId = 6,
-                Nom = "Yogourt Activia",
-                Qty = 150,
-                Prix = 3,
-                Image = "activia.jpg",
-                EstActif = true,
-                Note = 4,
-                ValeurNutritive = "Calories: 100, Lipides: 2g, Glucides: 15g, Sucres: 12g, Protéines: 5g"
-            },
-            new Produit
-            {
-                ProduitId = 7,
-                Nom = "Pizza congelée",
-                Qty = 60,
-                Prix = 6,
-                Image = "pizza.jpg",
-                EstActif = true,
-                Note = 4,
-                ValeurNutritive = "Calories: 350, Lipides: 15g, Glucides: 40g, Sucres: 5g, Protéines: 12g"
-            },
-            new Produit
-            {
-                ProduitId = 8,
-                Nom = "Barre de granola",
-                Qty = 180,
-                Prix = 2,
-                Image = "granola.jpg",
-                EstActif = true,
-                Note = 4,
-                ValeurNutritive = "Calories: 190, Lipides: 7g, Glucides: 29g, Sucres: 11g, Protéines: 4g"
-            },
-            new Produit
-            {
-                ProduitId = 9,
-                Nom = "Coca-Cola",
-                Qty = 220,
-                Prix = 2,
-                Image = "coca.jpg",
-                EstActif = true,
-                Note = 3,
-                ValeurNutritive = "Calories: 140, Sucres: 39g, Glucides: 39g, Sodium: 45mg"
-            },
-            new Produit
-            {
-                ProduitId = 10,
-                Nom = "Sandwich jambon-fromage",
-                Qty = 75,
-                Prix = 4,
-                Image = "sandwich.jpg",
-                EstActif = true,
-                Note = 4,
-                ValeurNutritive = "Calories: 320, Lipides: 12g, Glucides: 30g, Protéines: 18g, Sodium: 780mg"
-            }
-
+                new Produit
+                {
+                    ProduitId = 1,
+                    Nom = "Red Bull",
+                    Qty = 120,
+                    Prix = 3,
+                    Image = "redbull.png",
+                    EstActif = true,
+                    Note = 4,
+                    ValeurNutritive = "Calories: 110, Sucres: 27g, Caféine: 80mg, Glucides: 28g, Protéines: 1g",
+                    DateAjout = DateTime.Now.AddMonths(-2) // 👴
+                },
+                new Produit
+                {
+                    ProduitId = 2,
+                    Nom = "Pogo",
+                    Qty = 200,
+                    Prix = 2,
+                    Image = "pogo.jpg",
+                    EstActif = true,
+                    Note = 3,
+                    ValeurNutritive = "Calories: 190, Lipides: 9g, Glucides: 20g, Protéines: 6g, Sodium: 500mg",
+                    DateAjout = DateTime.Now.AddMonths(-2)
+                },
+                new Produit
+                {
+                    ProduitId = 3,
+                    Nom = "Bouteille d'eau",
+                    Qty = 300,
+                    Prix = 1,
+                    Image = "eau.jpg",
+                    EstActif = true,
+                    Note = 5,
+                    ValeurNutritive = "Calories: 0, Lipides: 0g, Sucres: 0g, Sodium: 0mg",
+                    DateAjout = DateTime.Now.AddMonths(-2)
+                },
+                new Produit
+                {
+                    ProduitId = 4,
+                    Nom = "Chips Lay’s",
+                    Qty = 100,
+                    Prix = 2,
+                    Image = "chips.jpg",
+                    EstActif = true,
+                    Note = 4,
+                    ValeurNutritive = "Calories: 160, Lipides: 10g, Glucides: 15g, Sucres: 1g, Sodium: 170mg",
+                    DateAjout = DateTime.Now.AddMonths(-2)
+                },
+                new Produit
+                {
+                    ProduitId = 5,
+                    Nom = "Nutella",
+                    Qty = 80,
+                    Prix = 5,
+                    Image = "nutella.jpg",
+                    EstActif = true,
+                    Note = 5,
+                    ValeurNutritive = "Calories: 200, Lipides: 11g, Glucides: 22g, Sucres: 21g, Protéines: 2g",
+                    DateAjout = DateTime.Now.AddMonths(-2)
+                },
+                new Produit
+                {
+                    ProduitId = 6,
+                    Nom = "Yogourt Activia",
+                    Qty = 150,
+                    Prix = 3,
+                    Image = "activia.jpg",
+                    EstActif = true,
+                    Note = 4,
+                    ValeurNutritive = "Calories: 100, Lipides: 2g, Glucides: 15g, Sucres: 12g, Protéines: 5g",
+                    DateAjout = DateTime.Now.AddMonths(-2)
+                },
+                new Produit
+                {
+                    ProduitId = 7,
+                    Nom = "Pizza congelée",
+                    Qty = 60,
+                    Prix = 6,
+                    Image = "pizza.jpg",
+                    EstActif = true,
+                    Note = 4,
+                    ValeurNutritive = "Calories: 350, Lipides: 15g, Glucides: 40g, Sucres: 5g, Protéines: 12g",
+                    DateAjout = DateTime.Now.AddMonths(-2)
+                },
+                new Produit
+                {
+                    ProduitId = 8,
+                    Nom = "Barre de granola",
+                    Qty = 180,
+                    Prix = 2,
+                    Image = "granola.jpg",
+                    EstActif = true,
+                    Note = 4,
+                    ValeurNutritive = "Calories: 190, Lipides: 7g, Glucides: 29g, Sucres: 11g, Protéines: 4g",
+                    DateAjout = DateTime.Now.AddMonths(-2)
+                },
+                new Produit
+                {
+                    ProduitId = 9,
+                    Nom = "Coca-Cola",
+                    Qty = 220,
+                    Prix = 2,
+                    Image = "coca.jpg",
+                    EstActif = true,
+                    Note = 3,
+                    ValeurNutritive = "Calories: 140, Sucres: 39g, Glucides: 39g, Sodium: 45mg",
+                    DateAjout = DateTime.Now 
+                },
+                new Produit
+                {
+                    ProduitId = 10,
+                    Nom = "Sandwich jambon-fromage",
+                    Qty = 75,
+                    Prix = 4,
+                    Image = "sandwich.jpg",
+                    EstActif = true,
+                    Note = 4,
+                    ValeurNutritive = "Calories: 320, Lipides: 12g, Glucides: 30g, Protéines: 18g, Sodium: 780mg",
+                    DateAjout = DateTime.Now 
+                }
             );
+            #endregion
+
         }
     }
 }
---
+++ E25ProjetEtendu/E25ProjetEtendu/Migrations/20250507154354_ajoutDateProduit.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace E25ProjetEtendu.Migrations
+{
+    /// <inheritdoc />
+    public partial class ajoutDateProduit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateAjout",
+                table: "produits",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "21111111-1111-1111-1111-111111111111",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "04e70b73-7118-4885-8685-b8d3f9d6dcdb", "AQAAAAIAAYagAAAAEGqfhiNjWlbuHkoWzKP3MqJMqAB0lgaQChCUnPXNscRD8d4UMUS5Ibcs1VU0LT8ntQ==", "b7be0fac-e3e0-4c01-a6d8-159ce8900e56" });
+
+            migrationBuilder.UpdateData(
+                table: "produits",
+                keyColumn: "ProduitId",
+                keyValue: 1,
+                column: "DateAjout",
+                value: new DateTime(2025, 3, 7, 11, 43, 52, 910, DateTimeKind.Local).AddTicks(4723));
+
+            migrationBuilder.UpdateData(
+                table: "produits",
+                keyColumn: "ProduitId",
+                keyValue: 2,
+                column: "DateAjout",
+                value: new DateTime(2025, 3, 7, 11, 43, 52, 910, DateTimeKind.Local).AddTicks(4811));
+
+            migrationBuilder.UpdateData(
+                table: "produits",
+                keyColumn: "ProduitId",
+                keyValue: 3,
+                column: "DateAjout",
+                value: new DateTime(2025, 3, 7, 11, 43, 52, 910, DateTimeKind.Local).AddTicks(4819));
+
+            migrationBuilder.UpdateData(
+                table: "produits",
+                keyColumn: "ProduitId",
+                keyValue: 4,
+                column: "DateAjout",
+                value: new DateTime(2025, 3, 7, 11, 43, 52, 910, DateTimeKind.Local).AddTicks(4823));
+
+            migrationBuilder.UpdateData(
+                table: "produits",
+                keyColumn: "ProduitId",
+                keyValue: 5,
+                column: "DateAjout",
+                value: new DateTime(2025, 3, 7, 11, 43, 52, 910, DateTimeKind.Local).AddTicks(4826));
+
+            migrationBuilder.UpdateData(
+                table: "produits",
+                keyColumn: "ProduitId",
+                keyValue: 6,
+                column: "DateAjout",
+                value: new DateTime(2025, 3, 7, 11, 43, 52, 910, DateTimeKind.Local).AddTicks(4829));
+
+            migrationBuilder.UpdateData(
+                table: "produits",
+                keyColumn: "ProduitId",
+                keyValue: 7,
+                column: "DateAjout",
+                value: new DateTime(2025, 3, 7, 11, 43, 52, 910, DateTimeKind.Local).AddTicks(4833));
+
+            migrationBuilder.UpdateData(
+                table: "produits",
+                keyColumn: "ProduitId",
+                keyValue: 8,
+                column: "DateAjout",
+                value: new DateTime(2025, 3, 7, 11, 43, 52, 910, DateTimeKind.Local).AddTicks(4836));
+
+            migrationBuilder.UpdateData(
+                table: "produits",
+                keyColumn: "ProduitId",
+                keyValue: 9,
+                column: "DateAjout",
+                value: new DateTime(2025, 5, 7, 11, 43, 52, 910, DateTimeKind.Local).AddTicks(4839));
+
+            migrationBuilder.UpdateData(
+                table: "produits",
+                keyColumn: "ProduitId",
+                keyValue: 10,
+                column: "DateAjout",
+                value: new DateTime(2025, 5, 7, 11, 43, 52, 910, DateTimeKind.Local).AddTicks(4843));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateAjout",
+                table: "produits");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "21111111-1111-1111-1111-111111111111",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "201c30b8-53a3-460d-8f23-a65c1d63f1e8", "AQAAAAIAAYagAAAAEAWzwizuk96riR16iNbPvI/NYG0KCtDPML1GibAV2Zz6fY2nwVMiFR7vpbpPQD1x7Q==", "9b5faa6f-b44f-4932-8dd1-f7f478e07680" });
+        }
+    }
+}
