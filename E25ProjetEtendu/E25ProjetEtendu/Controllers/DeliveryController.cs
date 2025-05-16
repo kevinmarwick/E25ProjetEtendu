dd979af208a3659c5c2536549a717d04e891cfaf
gab <6221324@cegepmontpetit.ca>
2025-05-16 12:31:31 -0400

    FCT Accepter une commande
    permet a un utilisateur d'Accepter un commande en s'authentifiant

--- E25ProjetEtendu/E25ProjetEtendu/Controllers/DeliveryController.cs
+++ E25ProjetEtendu/E25ProjetEtendu/Controllers/DeliveryController.cs
@@ -1,6 +1,11 @@
-﻿using E25ProjetEtendu.Services.IServices;
+﻿using E25ProjetEtendu.Data;
+using E25ProjetEtendu.Models;
+using E25ProjetEtendu.Services.IServices;
+using E25ProjetEtendu.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace E25ProjetEtendu.Controllers
 {
@@ -8,17 +13,64 @@ namespace E25ProjetEtendu.Controllers
     public class DeliveryController : Controller
     {
         private readonly IDeliveryService _deliveryService;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        
 
-        public DeliveryController(IDeliveryService deliveryService)
+        public DeliveryController(IDeliveryService deliveryService, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ApplicationDbContext dbContext)
         {
             _deliveryService = deliveryService;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            
+
         }
 
         public async Task<IActionResult> Index()
         {
-            var orders = await _deliveryService.GetUnassignedOrder();
-            return View(orders);
+            var nextOrder = await _deliveryService.GetUnassignedOrder();
+            var assignedOrders = await _deliveryService.GetAssignedOrders();
+
+            var vm = new DeliveryVM
+            {
+                NextOrder = nextOrder,
+                AssignedOrders = assignedOrders
+            };
+
+            return View(vm);
         }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> ConfirmDelivery(int orderId, string email, string password)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                ModelState.AddModelError("", "Utilisateur non trouvé.");
+                return RedirectToAction("Index");
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, password, false);
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Mot de passe invalide.");
+                return RedirectToAction("Index");
+            }
+
+            var assignationReussie = await _deliveryService.AssignerCommandeAuLivreur(orderId, user.Id);
+            if (!assignationReussie)
+            {
+                TempData["Erreur"] = "La commande n'est plus disponible.";
+                return RedirectToAction("Index");
+            }
+
+            TempData["Succès"] = "Commande acceptée avec succès.";
+            return RedirectToAction("Index"); 
+        }
+
+
     }
 
 }
