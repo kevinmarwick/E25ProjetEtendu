408a33a02ce6367d9e87aa92785d398a2fc51359
Kevin Marwick <6107320@cegepmontpetit.ca>
2025-05-31 23:41:46 -0400

    Changement contenu Lien par SMS

--- E25ProjetEtendu/E25ProjetEtendu/Services/smsService.cs
+++ E25ProjetEtendu/E25ProjetEtendu/Services/smsService.cs
@@ -1,23 +1,30 @@
 using Twilio;
 using Twilio.Rest.Api.V2010.Account;
 using Twilio.Types;
+using E25ProjetEtendu.Models;
+using E25ProjetEtendu.Services.IServices;
 
 public class SmsService
 {
     private readonly HttpClient _httpClient;
    
 
-    public SmsService()
+    private readonly IOrderService _orderService;
+
+    public SmsService(IOrderService orderService)
     {
         //TwilioClient.Init(_accountSid, _authToken);
+        _orderService = orderService;
     }
 
     public async Task<bool> EnvoyerLienConfirmationAuLivreur(string phoneNumber, int orderId)
     {
+        Order? order = await _orderService.GetOrderById(orderId);
+
         try
         {
             var confirmationUrl = $"https://localhost:7135/Order/EndOrder?orderId={orderId}";
-            var messageText = $"Votre commande a été livrée. Cliquez ici pour la confirmer : {confirmationUrl}";
+            var messageText = $"La commande {orderId} pour {order.Buyer.FullName} vous a bien été assignée. Cliquez ici pour la conclure ou pour l'annuler : {confirmationUrl}";
 
             var message = await MessageResource.CreateAsync(
                 to: new PhoneNumber(phoneNumber),
