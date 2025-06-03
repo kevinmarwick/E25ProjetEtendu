03f8d5d00265884c3b3fa4de4a6a033f2b3390d7
Gabriel <6221324@cegepmontpetit.ca>
2025-06-03 15:12:23 -0400

    FCT Service/Controller Category

---
+++ E25ProjetEtendu/E25ProjetEtendu/Services/CategoryService.cs
@@ -0,0 +1,14 @@
+﻿using E25ProjetEtendu.Data;
+using E25ProjetEtendu.Services.IServices;
+
+namespace E25ProjetEtendu.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
